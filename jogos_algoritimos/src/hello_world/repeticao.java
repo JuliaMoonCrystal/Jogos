f51e913ed6ad7dc008cf62a218f76ceaa7190d69
Julia Farias da Rocha <jufarias20002@gmail.com>
2024-03-31 12:44:25 -0300

    classe repeticao

--- jogos_algoritimos/src/hello_world/repeticao.java
+++ jogos_algoritimos/src/hello_world/repeticao.java
@@ -1,9 +1,30 @@
 package hello_world;
 
+import java.util.Iterator;
+
 public class repeticao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int cont =1;
+		
+		while (cont != 10) {
+			 System.out.print("Número cont:" +cont+"\n");
+			cont ++;
+			
+			
+		}
+		
+		do {
+			 System.out.print("Número cont do while:" +cont+"\n");
+			cont ++;
+		} while (cont <= 10);
+		
+		for (int i = 1;i <=10;i++) {
+			 System.out.print("Número cont for" +i+"\n");
+			
+		}
+		
+		
 
 	}
 
