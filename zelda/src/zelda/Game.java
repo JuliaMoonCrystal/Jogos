d5eb549719e11019f965edcdecd437079e611545
Julia Farias da Rocha <jufarias20002@gmail.com>
2024-03-31 20:32:44 -0300

    add Player class

--- zelda/src/zelda/Game.java
+++ zelda/src/zelda/Game.java
@@ -1,12 +1,18 @@
 package zelda;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable,KeyListener{
 	
+	public Player player;
 	
 	@Override
 	public void run() {
@@ -14,7 +20,7 @@ public class Game extends Canvas implements Runnable{
 			tick();
 			render();
 			try {
-				Thread.sleep(1000/50);
+				Thread.sleep(1000/60);
 			} catch (InterruptedException e) {
 				
 				e.printStackTrace();
@@ -25,15 +31,32 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public Game () {
+		this.addKeyListener(this);
 		this.setPreferredSize(new Dimension(480,480));
+		
+		player = new Player(0, 0);
 	}
 	
 	public void tick() {
-		
+		player.tick();
 	}
 	
 	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
 		
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 480, 480);
+		
+		player.render(g);
+		
+//		g.setColor(Color.red);
+//		g.fillRect(10, 10, 100, 100);
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
@@ -49,5 +72,47 @@ public class Game extends Canvas implements Runnable{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		new Thread(game).start();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.rigth=true;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left=true;
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up=true;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down=true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.rigth=false;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left=false;
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up=false;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down=false;
+		}
+		
 	}
 }
